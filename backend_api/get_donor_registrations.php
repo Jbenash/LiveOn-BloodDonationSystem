b08f21a5b34cc8c3fda0a46335c90030eceb37f1
Jbenash <mbenash961030@gmail.com>
2025-07-13 23:46:20 +0530

    registration form hospital issue resolved

--- backend_api/get_donor_registrations.php
+++ backend_api/get_donor_registrations.php
@@ -25,22 +25,22 @@ if ($conn->connect_error) {
 }
 
 $sql = "SELECT d.donor_id, u.name AS full_name, u.email, d.blood_type AS blood_group, 
-        d.address, d.city, d.last_donation_date, d.lives_saved, d.status, 
-        mv.verification_date AS verification_date
+        d.address, d.city, d.preferred_hospital_id, h.name AS preferred_hospital_name, d.last_donation_date, d.lives_saved, d.status
 FROM donors d
 INNER JOIN users u ON d.user_id = u.user_id
 INNER JOIN medical_verifications mv ON d.donor_id = mv.donor_id
+LEFT JOIN hospitals h ON d.preferred_hospital_id = h.hospital_id
 WHERE u.role = 'donor'
 ORDER BY mv.verification_date DESC";
 $result = $conn->query($sql);
 
 $donors = [];
 if ($result && $result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $donors[] = $row;
     }
 }
 
 $conn->close();
 
-echo json_encode($donors); 
+echo json_encode($donors);
